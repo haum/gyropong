91a89d3b3c5760bcd9c684b2617a0d66a58e06d4
JackDesBwa <jackdesbwa@gmail.com>
2021-08-11 15:42:40 +0200

    Fix scale in paddle touch test

--- game/sprites/paddle.py
+++ game/sprites/paddle.py
@@ -31,8 +31,8 @@ class Paddle(pyglet.sprite.Sprite):
         sw, sh = gamestate['window'].width, gamestate['window'].height
         bx, by = ball.position
         ss = min(sw, sh) * 0.45
-        xcp, ycp = sw/2+(ss+412)*cos(self.angle), sh/2+(ss+412)*sin(self.angle)
-        if (bx-xcp)**2 + (by-ycp)**2 < 450**2:
+        xcp, ycp = sw/2+(ss+412*self.scale)*cos(self.angle), sh/2+(ss+412*self.scale)*sin(self.angle)
+        if (bx-xcp)**2 + (by-ycp)**2 < (450*self.scale)**2:
             n = atan2(by-ycp, bx-xcp)
             na = (n - self.angle) % (2 * pi)
             if na < pi + 0.254 and na > pi - 0.254:
